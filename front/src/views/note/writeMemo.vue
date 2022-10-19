256e51f1ee1e98bc392c38c5658c0c7c17360273
jeonjihyeong <spdlqj7014@naver.com>
2022-10-19 23:11:42 +0900

    add: write memo layout

---
+++ front/src/views/note/writeMemo.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container>   
+        <v-row class = "writeMemoTitle">
+            메모 작성하기
+        </v-row>
+        <v-row>
+            <v-textarea>
+                
+            </v-textarea>
+            <v-textarea>
+
+            </v-textarea>
+        </v-row>
+        <v-row>
+            댓글
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.writeMemoTitle{
+    font-size: 25px;
+    font-weight: bold;
+}
+</style>
