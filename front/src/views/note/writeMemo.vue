e9e869640e7406291ca21590bb68efcd3a4480b7
jeonjihyeong <102641699+jeonjihyeong@users.noreply.github.com>
2022-10-20 23:22:29 +0900

    add: writeMemo input

--- front/src/views/note/writeMemo.vue
+++ front/src/views/note/writeMemo.vue
@@ -3,23 +3,36 @@
         <v-row class = "writeMemoTitle">
             메모 작성하기
         </v-row>
+        <br><br>
         <v-row>
-            <v-textarea>
-                
-            </v-textarea>
-            <v-textarea>
-
-            </v-textarea>
-        </v-row>
+            <v-text-field
+                outlined
+                label="제목"
+                v-model="content" 
+                hide-details="false"
+                class = 'writeMemoInputTitle mb-2'
+            ></v-text-field>
+        </v-row>    
         <v-row>
-            댓글
+            <v-textarea
+                outlined
+                label="내용"
+                v-model="content" 
+                class="writeMemoInputContent"
+                height="500px"
+            ></v-textarea>
         </v-row>
     </v-container>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                title: '',
+                content: '',
+            }
+        },
     }
 </script>
 
