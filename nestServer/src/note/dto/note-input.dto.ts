8bae0a0d45af5f45cc716634f9ef953f85bbeb06
jeonjihyeong <spdlqj7014@naver.com>
2023-01-12 22:13:45 +0900

    **01.12**
    1. typeORM 커스텀 레포지토리

--- nestServer/src/note/dto/note-input.dto.ts
+++ nestServer/src/note/dto/note-input.dto.ts
@@ -1,10 +1,13 @@
-import {IsString } from 'class-validator';
+import {IsNotEmpty, IsString } from 'class-validator';
 
 export class NoteInputDto {
     @IsString()
+    @IsNotEmpty()
     title:string;
+
     
     @IsString()
+    @IsNotEmpty()
     content:string;
 }
 
--- nestServer/src/note/note.module.ts
+++ nestServer/src/note/note.module.ts
@@ -3,10 +3,17 @@ import { NoteService } from './note.service';
 import { NoteController } from './note.controller';
 import { NoteRepository } from './note.repo';
 import { TypeOrmExModule } from 'src/db/typeorm-module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Note } from './entities/note.entity';
 
 @Module({
-  imports : [TypeOrmExModule.forCustomRepository([NoteRepository])],
+  imports : [
+    // TypeOrmModule.forFeature([Note]),
+    TypeOrmModule.forFeature([Note])
+    // TypeOrmExModule.forCustomRepository([NoteRepository])
+  ],
+  // exports:[TypeOrmModule],
   providers: [NoteService],
-  controllers: [NoteController]
+  controllers: [NoteController],
 })
 export class NoteModule {}
