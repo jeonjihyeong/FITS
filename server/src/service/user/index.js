21ff972b6e843fea86eba3c5aa1b71f3d24880e7
jeonjihyeong <spdlqj7014@naver.com>
2023-02-19 01:11:38 +0900

    **02.19**
    1. 노트북으로 코드 옮기기용 커밋

--- server/src/service/user/index.js
+++ server/src/service/user/index.js
@@ -1,5 +1,6 @@
 const { connection_error } = require("../../lib/common/error")
 const redisClient = require("../../lib/common/redis.util")
+const { userRepo } = require("../../reposiotory")
 
 const logout=async(id)=>{
     let logOutResult
@@ -9,6 +10,23 @@ const logout=async(id)=>{
         throw new Error(connection_error.SERVICE_DELETE_USER_IP_ERROR)
     }
     return logOutResult
+}
+
+const follow = async(follower,following)=>{
+    await _checkFollow(follower,following)
+    try{
+        
+    }catch(err){
+
+    }
+}
+
+const _checkFollow = async(follower,following) => {
+    try{
+        await userRepo.checkFollow(follower,following)
+    }catch(err){
+
+    }
 }
 
 module.exports={
