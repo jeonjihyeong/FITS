2f99a47c27207b7c76a80b09b008d38b40f2730d
jeonjihyeong <102641699+jeonjihyeong@users.noreply.github.com>
2023-02-26 22:52:38 +0900

    **02.26**
    1. follow unfollow service layer

--- server/src/service/user/index.js
+++ server/src/service/user/index.js
@@ -17,7 +17,18 @@ const follow = async(follower,following)=>{
     try{
         
     }catch(err){
+        if(err.message)throw new Error(err.message)
+        throw new Error(connection_error.SERVICE_SET_FOLLOW_ERROR)
+    }
+}
 
+const unFollow = async(follower,following)=>{
+    await _checkFollow(follower,following)
+    try{
+        
+    }catch(err){
+        if(err.message)throw new Error(err.message)
+        throw new Error(connection_error.SERVICE_DELETE_FOLLOW_ERROR)
     }
 }
 
@@ -25,10 +36,13 @@ const _checkFollow = async(follower,following) => {
     try{
         await userRepo.checkFollow(follower,following)
     }catch(err){
-
+        if(err.message)throw new Error(err.message)
+        throw new Error(connection_error.SERVICE_CHECK_FOLLOW_ERROR)
     }
 }
 
 module.exports={
-    logout
+    logout,
+    follow,
+    unFollow,
 }
