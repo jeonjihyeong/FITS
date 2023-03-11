700843efa6f2c3b229f772dcdbbfac24e2ca3a28
jeonjihyeong <spdlqj7014@naver.com>
2023-03-11 20:51:22 +0900

    **03.11**
    1. 프론트 단에서의 에러 핸들링 axios interceptor 를 통해 status 에 따른 에러 처리( alert로 에러 띄워주기)

--- front/src/api/axios/index.js
+++ front/src/api/axios/index.js
@@ -1,5 +1,11 @@
 import axios from 'axios'
 
+const error_message = {
+    connectionError : "Connection Error",
+    netWorkError: "Network Error",
+    notFoundError: "404 Not Found Error"
+}
+
 const instance = axios.create({
     baseURL: process.env.VUE_APP_SERVER_URL,
 });
@@ -30,32 +36,45 @@ instance.interceptors.response.use(
     },
     async function(error){
         const {
-            config,
+            // config,
             response:{status},
         }=error;
-        if(status===419){
-            const originalRequest = config;
-            console.log(error.response.status);
-            const accessToken = localStorage.getItem('accessToken')
-            const refreshToken = localStorage.getItem('refreshToken')
-            try{
-                await axios.get(`${process.env.VUE_APP_SERVER_URL}/refresh`,{
-                    headers:{
-                        authorization:accessToken,
-                        refresh:refreshToken
-                    }
-                })
-            }catch(err){
-                console.log(err);
-            }
-            console.log("refreshing")
-            localStorage.setItem("accessToken",res.data.token.accessToken);
-            localStorage.setItem("refreshToken",res.data.token.refreshToken);
-            axios.defaults.headers.common.Authorization = res.data.token.accessToken;
-            originalRequest.headers.Authorization = res.data.token.accessToken;
-            const res = await axios(originalRequest)
-            return res
+        if(status===404){
+            alert(error_message.notFoundError)
         }
+
+        if(status===500){
+            alert(error_message.connectionError)
+        }
+
+        if(error.message===error_message.netWorkError){
+            alert(error_message.netWorkError)
+        }
+
+        //TODO: JWT Refresh Token 관련 구현 필요
+        // if(status===419){
+        //     const originalRequest = config;
+        //     console.log(error.response.status);
+        //     const accessToken = localStorage.getItem('accessToken')
+        //     const refreshToken = localStorage.getItem('refreshToken')
+        //     try{
+        //         await axios.get(`${process.env.VUE_APP_SERVER_URL}/refresh`,{
+        //             headers:{
+        //                 authorization:accessToken,
+        //                 refresh:refreshToken
+        //             }
+        //         })
+        //     }catch(err){
+        //         console.log(err);
+        //     }
+        //     console.log("refreshing")
+        //     localStorage.setItem("accessToken",res.data.token.accessToken);
+        //     localStorage.setItem("refreshToken",res.data.token.refreshToken);
+        //     axios.defaults.headers.common.Authorization = res.data.token.accessToken;
+        //     originalRequest.headers.Authorization = res.data.token.accessToken;
+        //     const res = await axios(originalRequest)
+        //     return res
+        // }
         return Promise.reject(error);
     }
 )
